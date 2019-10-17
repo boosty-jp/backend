5b405b312e847a1310db905668c7c1f1bf0a10ea
tomokiya <t.elle.hormon@gmail.com>
2019-10-17 22:16:21 +0900

    サービスクラス作成

---
+++ src/main/java/co/jp/wever/graphql/domain/service/section/DeleteSectionService.java
@@ -0,0 +1,4 @@
+package co.jp.wever.graphql.domain.service.section;
+
+public class DeleteSectionService {
+}
---
+++ src/main/java/co/jp/wever/graphql/domain/service/section/UpdateSectionService.java
@@ -0,0 +1,8 @@
+package co.jp.wever.graphql.domain.service.section;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UpdateSectionService {
+}
+
